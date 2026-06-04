f825041dc3d24b29ee795b36f388c8c628fab324
saheb kumar <sk9128050035@gmail.com>
2026-06-04 23:01:09 +0530

    add brevo con

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
