b34718c2fa7dc0364877b0da15be096584ca71f6
saheb kumar <sk9128050035@gmail.com>
2026-06-04 23:43:25 +0530

    add brevo mail

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -74,3 +74,35 @@ export const sendDelOtpMail = async (user, otp) => {
     throw error;
   }   
 };
+
+// import nodemailer from "nodemailer";
+// import dotenv from "dotenv";
+// dotenv.config();
+// // Create a transporter using SMTP
+// const transporter = nodemailer.createTransport({
+//   service: "Gmail",
+//   port: 465,
+//   secure: true, // use STARTTLS (upgrade connection to TLS after connecting)
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS,
+//   },
+// });
+
+// export const sendOtpMail = async (to, otp) => {
+//   await transporter.sendMail({
+//     from: process.env.SMTP_USER,
+//     to,
+//     subject: "Your OTP for Spicesprint",
+//     text: `Your OTP for Spicesprint is: ${otp}`,
+//   })
+// }
+
+// export const sendDelOtpMail = async (user, otp) => {
+//   await transporter.sendMail({
+//     from: process.env.SMTP_USER,
+//     to:user.email,
+//     subject: "Delivery Otp",
+//     text: `Your OTP for Delivery is: ${otp}`,
+//   })
+// }
